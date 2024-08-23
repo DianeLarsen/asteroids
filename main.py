6cd667289ae27ef8891756691529ac9e99baf486
DianeLarsen <diane.larsen@outlook.com>
2024-08-23 09:41:23 -0700

    finished basic game

--- main.py
+++ main.py
@@ -35,7 +35,11 @@ def main():
                 return
         for obj in updatable:
             obj.update(dt)
-   
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
         for asteroid in asteroids:
             if asteroid.collides_with(player):
                 print("Game over!")
